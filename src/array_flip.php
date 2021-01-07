365b730e6b423a7b4621dda4685df062027e10d3
fumiya5863 <fumiya5863@users.noreply.github.com>
2021-01-07 20:39:06 +0900

    #1 Add array_change_key_case and array_flip

--- src/array_flip.php
+++ src/array_flip.php
@@ -1,44 +1,46 @@
 <?php
 
 /*=============================================================================*/
-/*  array_flip(array $array): array
-    配列のキーと値を反転する  */
+/*  array_flip ( array $array ) : array
+    array_flip — Exchanges all keys with their associated values in an array  */
 /*=============================================================================*/
 
 $input = array("oranges", "apples", "pears");
-// $flipped = array_flip($input);
-// print_r($flipped);
 
-// 方法1
-// function flip($array) {
-//     $result = [];
-//     foreach($array as $key => $value) {
-//         if (!is_string($key) && !is_int($key)) {
-//             continue;
-//         }
-//         $result[$value] = $key;
-//     }
-//     return $result;
-// }
-// $result = flip($input);
-// print_r($result);
+// Built-in functions
+$flipped = array_flip($input);
+print_r($flipped);
 
-// 方法2(再帰処理)
-// function _flip($array, $keys, $num) {
-//     $result = [];
-//     $key = $keys[$num];
-//     $value = array_shift($array);
-//     $result[$value] = $key;
-//     $num++;
-//     if (count($array) > 0) {
-//         $result = $result + _flip($array, $keys, $num);
-//     }
-//     return $result;
-// }
-// $result = _flip($input, array_keys($input), 0);
-// print_r($result);
+// Method 1
+function flip($array) {
+    $result = [];
+    foreach($array as $key => $value) {
+        if (!is_string($key) && !is_int($key)) {
+            continue;
+        }
+        $result[$value] = $key;
+    }
+    return $result;
+}
+$result = flip($input);
+print_r($result);
 
-// 方法2-1(再帰処理)
+// Method 2(Recursive function)
+function _flip($array, $keys, $num) {
+    $result = [];
+    $key = $keys[$num];
+    $value = array_shift($array);
+    $result[$value] = $key;
+    $num++;
+    if (count($array) > 0) {
+        $result = $result + _flip($array, $keys, $num);
+    }
+    return $result;
+}
+$result = _flip($input, array_keys($input), 0);
+print_r($result);
+
+// Method 2-1(Recursive function)
 // function _flip($array, $num) {
 //     $result = [];
 //     $keys = array_keys($array);
@@ -54,19 +56,43 @@ $input = array("oranges", "apples", "pears");
 // $result = _flip($input, 0);
 // print_r($result);
 
-// 方法2-2(再帰処理)
-function _flip($array) {
-    $result = [];
-    $key = array_key_first($array);
-    $value = array_shift($array);
-    $result[$value] = $key;
+// Method 2-2(Recursive function)
+// function _flip($array) {
+//     $result = [];
+//     $key = array_key_first($array);
+//     $value = array_shift($array);
+//     $result[$value] = $key;
     
-    if (count($array) > 0) {
-        $result = $result + _flip($array);
+//     if (count($array) > 0) {
+//         $result = $result + _flip($array);
+//     }
+//     return $result;
+// }
+// $result = _flip($input);
+// print_r($result);
+
+// Method 3(Recursive function+internal pointer operation+passing by reference)
+function getKeyAndValue(&$array) {
+    $value = current($array);
+    $key = key($array);
+    next($array);
+    return [
+        "key" => $key,
+        "value" => $value
+    ];
+}
+function confirmLastValue($array, $value) {
+    return $array[count($array)-1] === $value;
+}
+
+function __flip($array) {
+    $result = [];
+    $keyAndvalue = getKeyAndValue($array);
+    $result[$keyAndvalue["value"]] = $keyAndvalue["key"];
+    if (!confirmLastValue($array, $keyAndvalue["value"])) {
+        $result = $result + __flip($array);
     }
     return $result;
 }
-$result = _flip($input);
+$result = __flip($input);
 print_r($result);
-
-// 方法3(再帰処理+内部ポインタ+参照渡し)
